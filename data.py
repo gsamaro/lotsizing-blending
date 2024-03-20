a56038000f3c4f92c9b3a38d63fc70925e36194f
Gabriel Amaro <53010587+gsamaro@users.noreply.github.com>
2024-03-20 14:11:28 -0300

    init commit

---
+++ data.py
@@ -0,0 +1,77 @@
+from abc import ABC, abstractmethod
+from typing import Any, Dict
+from numpy import ndarray
+import numpy as np
+
+
+class ReadData(ABC):
+    @abstractmethod
+    def __init__(self, file_to_read: str):
+        pass
+
+
+class Data(ReadData):
+    PERIODS: ndarray[Any, int]
+    END_PRODUCTS: ndarray[Any, int]
+    INGREDIENTS: ndarray[Any, int]
+    INGREDIENTS_OF_PRODUCT: Dict[int, int]
+    ub: ndarray[(Any, Any), float]
+    lb: ndarray[(Any, Any), float]
+    demand_end: ndarray[(Any, Any), float]
+    sum_demand_end: ndarray[(Any, Any), float]
+    holding_cost_end: ndarray[(Any, Any), float]
+    setup_cost_end: ndarray[(Any, Any), float]
+    production_cost_end: ndarray[(Any, Any), float]
+    production_time_end: ndarray[(Any, Any), float]
+    setup_time_end: ndarray[(Any, Any), float]
+    capacity_end: ndarray[(Any, Any), float]
+    holding_cost_ingredient: ndarray[(Any, Any), float]
+    setup_cost_ingredient: ndarray[(Any, Any), float]
+    production_cost_ingredient: ndarray[(Any, Any), float]
+    file_to_read: str
+
+    def __init__(self, file_to_read: str):
+        self.file_to_read = file_to_read
+        pass
+
+
+class MockData(Data):
+    def __init__(self, file_to_read: str):
+        super().__init__("mock")
+        self.PERIODS = np.arange(1)
+        self.END_PRODUCTS = np.arange(2)
+        self.INGREDIENTS = np.arange(4)
+        self.ub = np.array([[0.3, 0], [0.7, 0], [0.2, 0.5], [0.1, 0.8]])
+        self.lb = np.array([[0.3, 0], [0.5, 0], [0.0, 0.2], [0.1, 0.8]])
+        self.demand_end = np.array([[1], [1]])
+        self.sum_demand_end = np.array([[[1]],[[1]]])
+        self.holding_cost_end = np.full(
+            shape=(self.END_PRODUCTS.shape[0], self.PERIODS.shape[0]), fill_value=1
+        )
+        self.setup_cost_end = np.full(
+            shape=(self.END_PRODUCTS.shape[0], self.PERIODS.shape[0]), fill_value=1
+        )
+        self.production_cost_end = np.full(
+            shape=(self.END_PRODUCTS.shape[0], self.PERIODS.shape[0]), fill_value=0
+        )
+        self.production_time_end = np.full(
+            shape=(self.END_PRODUCTS.shape[0], self.PERIODS.shape[0]), fill_value=1
+        )
+        self.setup_time_end = np.full(
+            shape=(self.END_PRODUCTS.shape[0], self.PERIODS.shape[0]), fill_value=1
+        )
+        self.capacity_end = np.full(
+            shape=(self.END_PRODUCTS.shape[0], self.PERIODS.shape[0]), fill_value=10
+        )
+        self.holding_cost_ingredient = np.full(
+            shape=(self.INGREDIENTS.shape[0], self.PERIODS.shape[0]), fill_value=1
+        )
+        self.setup_cost_ingredient = np.full(
+            shape=(self.INGREDIENTS.shape[0], self.PERIODS.shape[0]), fill_value=1
+        )
+        self.production_cost_ingredient = np.full(
+            shape=(self.INGREDIENTS.shape[0], self.PERIODS.shape[0]), fill_value=0
+        )
+
+if __name__ == "__main__":
+    MockData("mock")
---
+++ formulacao.py
@@ -0,0 +1,261 @@
+from data import Data, MockData
+from docplex.mp.model import Model
+
+
+class Formulacao1:
+
+    def __init__(self, data: Data):
+        self.data = data
+        self.model = Model()
+        self.build_variables()
+
+        self.balance_inventory_end_products_constraint()
+        self.setup_end_products_constraint()
+        self.capacity_end_products_constraint()
+        self.balance_inventory_ingredients_constraint()
+        self.setup_ingredients_constraint()
+        self.upper_ingredients_constraint()
+        self.lower_ingredients_constraint()
+        self.total_proportion_end_products_constraint()
+
+        self.model.add_kpi(
+            self.setup_cost_end_products(), publish_name="setup_cost_end_products"
+        )
+        self.model.add_kpi(
+            self.production_cost_end_products(),
+            publish_name="production_cost_end_products",
+        )
+        self.model.add_kpi(
+            self.holding_cost_end_products(), publish_name="holding_cost_end_products"
+        )
+        self.model.add_kpi(self.end_products_cost(), publish_name="end_products_cost")
+
+        self.model.add_kpi(
+            self.setup_cost_ingredients(), publish_name="setup_cost_ingredients"
+        )
+        self.model.add_kpi(
+            self.production_cost_ingredients(),
+            publish_name="production_cost_ingredients",
+        )
+        self.model.add_kpi(
+            self.holding_cost_end_ingredients(),
+            publish_name="holding_cost_end_ingredients",
+        )
+        self.model.add_kpi(self.ingredients_cost(), publish_name="ingredients_cost")
+
+        self.model.minimize(self.end_products_cost() + self.ingredients_cost())
+
+    def build_variables(self):
+        self._build_end_products_var()
+        self._build_setup_end_products_var()
+        self._build_inventory_end_products_var()
+        self._build_ingredient_proportion_var()
+        self._build_ingredients_var()
+        self._build_setup_ingredients_var()
+        self._build_inventory_ingredients_var()
+
+    def _build_end_products_var(self):
+        self.end_products = self.model.continuous_var_matrix(
+            self.data.END_PRODUCTS.shape[0],
+            self.data.PERIODS.shape[0],
+            name="xE",
+        )
+
+    def _build_setup_end_products_var(self):
+        self.setup_end_products = self.model.binary_var_matrix(
+            self.data.END_PRODUCTS.shape[0],
+            self.data.PERIODS.shape[0],
+            name="yE",
+        )
+
+    def _build_inventory_end_products_var(self):
+        self.inventory_end_products = self.model.continuous_var_matrix(
+            self.data.END_PRODUCTS.shape[0],
+            self.data.PERIODS.shape[0],
+            name="sE",
+        )
+
+    def _build_ingredient_proportion_var(self):
+        self.ingredient_proportion = self.model.continuous_var_cube(
+            self.data.INGREDIENTS.shape[0],
+            self.data.END_PRODUCTS.shape[0],
+            self.data.PERIODS.shape[0],
+            name="p",
+        )
+
+    def _build_ingredients_var(self):
+        self.ingredients = self.model.continuous_var_matrix(
+            self.data.INGREDIENTS.shape[0],
+            self.data.PERIODS.shape[0],
+            name="x",
+        )
+
+    def _build_setup_ingredients_var(self):
+        self.setup_ingredients = self.model.binary_var_matrix(
+            self.data.INGREDIENTS.shape[0],
+            self.data.PERIODS.shape[0],
+            name="y",
+        )
+
+    def _build_inventory_ingredients_var(self):
+        self.inventory_ingredients = self.model.continuous_var_matrix(
+            self.data.INGREDIENTS.shape[0],
+            self.data.PERIODS.shape[0],
+            name="s",
+        )
+
+    def balance_inventory_end_products_constraint(self):
+        self.model.add_constraints(
+            self.end_products[k, 0]
+            == self.data.demand_end[k, 0] + self.inventory_end_products[k, 0]
+            for k in self.data.END_PRODUCTS
+        )
+
+        self.model.add_constraints(
+            self.inventory_end_products[k, t - 1] + self.end_products[k, t]
+            == self.data.demand_end[k, t] + self.inventory_end_products[k, t]
+            for k in self.data.END_PRODUCTS
+            for t in self.data.PERIODS[self.data.PERIODS > 0]
+        )
+
+    def setup_end_products_constraint(self):
+        self.model.add_constraints(
+            self.end_products[k, t]
+            <= self.data.sum_demand_end[k, t, self.data.PERIODS[-1]]
+            * self.setup_end_products[k, t]
+            for k in self.data.END_PRODUCTS
+            for t in self.data.PERIODS
+        )
+
+    def capacity_end_products_constraint(self):
+        self.model.add_constraints(
+            self.data.setup_time_end[k, t] * self.setup_end_products[k, t]
+            + self.data.production_time_end[k, t] * self.end_products[k, t]
+            <= self.data.capacity_end[k, t]
+            for k in self.data.END_PRODUCTS
+            for t in self.data.PERIODS
+        )
+
+    def balance_inventory_ingredients_constraint(self):
+        self.model.add_constraints(
+            self.ingredients[i, 0]
+            == self.model.sum(
+                self.ingredient_proportion[i, k, 0] for k in self.data.END_PRODUCTS
+            )
+            + self.inventory_ingredients[i, 0]
+            for i in self.data.INGREDIENTS
+        )
+
+        self.model.add_constraints(
+            self.inventory_ingredients[i, t - 1] + self.ingredients[i, t]
+            == self.model.sum(
+                self.ingredient_proportion[i, k, t] for k in self.data.END_PRODUCTS
+            )
+            + self.inventory_ingredients[i, t]
+            for i in self.data.INGREDIENTS
+            for t in self.data.PERIODS[self.data.PERIODS > 0]
+        )
+
+    def setup_ingredients_constraint(self):
+        self.model.add_constraints(
+            self.ingredients[i, t]
+            <= self.model.sum(self.data.ub[i, k] for k in self.data.END_PRODUCTS)
+            * self.model.sum(
+                self.data.sum_demand_end[k, t, self.data.PERIODS[-1]]
+                for k in self.data.END_PRODUCTS
+            )
+            for i in self.data.INGREDIENTS
+            for t in self.data.PERIODS
+        )
+
+    def upper_ingredients_constraint(self):
+        self.model.add_constraints(
+            self.ingredient_proportion[i, k, t]
+            <= self.data.ub[i, k] * self.end_products[k, t]
+            for k in self.data.END_PRODUCTS
+            for i in self.data.INGREDIENTS
+            for t in self.data.PERIODS
+        )
+
+    def lower_ingredients_constraint(self):
+        self.model.add_constraints(
+            self.ingredient_proportion[i, k, t]
+            >= self.data.lb[i, k] * self.end_products[k, t]
+            for k in self.data.END_PRODUCTS
+            for i in self.data.INGREDIENTS
+            for t in self.data.PERIODS
+        )
+
+    def total_proportion_end_products_constraint(self):
+        self.model.add_constraints(
+            self.model.sum(
+                self.ingredient_proportion[i, k, t] for i in self.data.INGREDIENTS
+            )
+            == self.end_products[k, t]
+            for k in self.data.END_PRODUCTS
+            for t in self.data.PERIODS
+        )
+
+    def setup_cost_end_products(self):
+        return self.model.sum(
+            self.data.setup_cost_end[k, t] * self.setup_end_products[k, t]
+            for k in self.data.END_PRODUCTS
+            for t in self.data.PERIODS
+        )
+
+    def production_cost_end_products(self):
+        return self.model.sum(
+            self.data.production_cost_end[k, t] * self.end_products[k, t]
+            for k in self.data.END_PRODUCTS
+            for t in self.data.PERIODS
+        )
+
+    def holding_cost_end_products(self):
+        return self.model.sum(
+            self.data.holding_cost_end[k, t] * self.inventory_end_products[k, t]
+            for k in self.data.END_PRODUCTS
+            for t in self.data.PERIODS
+        )
+
+    def end_products_cost(self):
+        return (
+            self.setup_cost_end_products()
+            + self.production_cost_end_products()
+            + self.holding_cost_end_products()
+        )
+
+    def setup_cost_ingredients(self):
+        return self.model.sum(
+            self.data.setup_cost_ingredient[i, t] * self.setup_ingredients[i, t]
+            for i in self.data.INGREDIENTS
+            for t in self.data.PERIODS
+        )
+
+    def production_cost_ingredients(self):
+        return self.model.sum(
+            self.data.production_cost_ingredient[i, t] * self.ingredients[i, t]
+            for i in self.data.INGREDIENTS
+            for t in self.data.PERIODS
+        )
+
+    def holding_cost_end_ingredients(self):
+        return self.model.sum(
+            self.data.holding_cost_ingredient[i, t] * self.inventory_ingredients[i, t]
+            for i in self.data.INGREDIENTS
+            for t in self.data.PERIODS
+        )
+
+    def ingredients_cost(self):
+        return (
+            self.setup_cost_ingredients()
+            + self.production_cost_ingredients()
+            + self.holding_cost_end_ingredients()
+        )
+
+
+if __name__ == "__main__":
+    data = MockData("F1.dat")
+    f1 = Formulacao1(data)
+    print(f1.model.export_as_lp_string())
+    solution = f1.model.solve()
+    f1.model.print_solution()
