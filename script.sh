edd5be4587b5a70e394278287696ec05549be4eb
Gabriel Amaro <gabriel.amaro94@gmail.com>
2024-03-20 16:06:26 -0300

    added new script for cluster

---
+++ script.sh
@@ -0,0 +1,17 @@
+#PBS -N main
+#PBS -q memlong
+#PBS -l nodes=1:ppn=128
+#PBS -e outputs/erros_memlong
+#PBS -o outputs/saidas_memlong
+#PBS -m abe
+#PBS -M g229780@dac.unicamp.br
+
+module load python/3.10.10-gcc-9.4.0
+module load openmpi/4.1.1-intel-2021.3.0
+source phd2/bin/activate
+
+cd $PBS_O_WORKDIR
+mpirun python -m mpi4py.futures main.py
+
+
+
