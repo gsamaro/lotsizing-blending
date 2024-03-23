6430e5184cb1e9e489310b9ce4de9872a38ad49c
Gabriel Amaro <gabriel.amaro94@gmail.com>
2024-03-23 17:59:17 -0300

    fix: added new instances in constant

--- constants.py
+++ constants.py
@@ -3,5 +3,8 @@ DEFAULT_CAPACITY_MULTIPLIER = {"N": 2, "L": 1.3}
 TIMELIMIT = 10
 OTIMIZADOS_INDIVIDUAIS_PATH = "SOLUTION/INDIVIDUAIS/"
 FINAL_PATH = "SOLUTION/FINAL/"
-END_PRODUCTS = [1, 2]
+END_PRODUCTS = [1, 2, 5]
 INSTANCES = ["2LLL1.DAT.dat", "2LLL3.DAT.dat"]
+INSTANCES = [f"{i}LLL{j}.DAT.dat" for i in range(1, 11) for j in range(1, 11)] + [
+    f"{i}HHH{j}.DAT.dat" for i in range(1, 11) for j in range(1, 11)
+]
