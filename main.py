7075bb8f529f096c2ba527a36316c48f737f3595
Gabriel Amaro <gabriel.amaro94@gmail.com>
2024-04-04 20:14:40 -0300

    add __main__

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from formulacao import Formulacao1
 from utils import running_all_instance_with_chosen_capacity
 
-running_all_instance_with_chosen_capacity(Formulacao1, path_to_save="formulacao1.xlsx")
+if __name__ == "__main__":
+    running_all_instance_with_chosen_capacity(
+        Formulacao1, path_to_save="formulacao1.xlsx"
+    )
